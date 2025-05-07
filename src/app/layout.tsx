3f8d3bbcf559c58bd35fa551c00ef074f12504af
mankhb2k <mankhb2k@gmail.com>
2025-05-07 14:38:22 +0700

    done next 15.3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,17 @@
+import '../styles/global.css'; // Import global.css
 
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {/* Layout UI */}
+        {/* Place children where you want to render a page or nested layout */}
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
